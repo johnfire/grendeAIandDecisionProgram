aa615a0a43cabf1fe2e48f4c4f5dc9d105b963bd
Christopher Rehm <christopherrehm@web.de>
2020-02-17 20:35:33 +0100

    basic layout

--- decisionProgram.py
+++ decisionProgram.py
@@ -1,4 +1,44 @@
 #!/usr/bin/python3
 
 #main decision program for grendel the robot
+class person():
+    name = ""
+    sex = ""
+    job = ""
+    hobbies = ()
+    location = ""
+
+class object():
+    name = ""
+    location = ""
+    function =""
+
+class myWorld ():
+    
+    def addNew():
+        pass
+
+
+    def changeLocation():
+        pass
+
+
+
+startEye1()
+startEye2()
+startConversationWindow()
+
+while (1):
+    # check for incoming info
+
+    # is info new info , to be analysed priority ??
+
+    #is info results  priority?
+
+    #either use results to do something
+
+    #or process new info
+
+
+    #repeat
 
